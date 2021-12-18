2410e2b653d57d5e517ded25b123c0a91d5f0092
Hakan Bilgin <hbi99@hotmail.com>
2021-12-19 00:56:32 +0100

    porting code from original

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,35 @@
 
+const Shaders = {
+	Fragment: {
+		advect: `@import "shaders/advect.frag"`,
+		splat: `@import "shaders/splat.frag"`,
+		divergence: `@import "shaders/divergence.frag"`,
+		jacobi: `@import "shaders/jacobi.frag"`,
+		subtract: `@import "shaders/subtract.frag"`,
+		resize: `@import "shaders/resize.frag"`,
+		project: `@import "shaders/project.frag"`,
+		brush: `@import "shaders/brush.frag"`,
+		painting: `@import "shaders/painting.frag"`,
+		picker: `@import "shaders/picker.frag"`,
+		panel: `@import "shaders/panel.frag"`,
+		output: `@import "shaders/output.frag"`,
+		shadow: `@import "shaders/shadow.frag"`,
+		setbristles: `@import "shaders/setbristles.frag"`,
+		updatevelocity: `@import "shaders/updatevelocity.frag"`,
+		planeconstraint: `@import "shaders/planeconstraint.frag"`,
+		bendingconstraint: `@import "shaders/bendingconstraint.frag"`,
+		distanceconstraint: `@import "shaders/distanceconstraint.frag"`,
+	},
+	Vertex: {
+		splat: `@import "shaders/splat.vert"`,
+		brush: `@import "shaders/brush.vert"`,
+		picker: `@import "shaders/picker.vert"`,
+		painting: `@import "shaders/painting.vert"`,
+		fullscreen: `@import "shaders/fullscreen.vert"`,
+	}
+};
+
+
 @import "modules/wrappedgl.js"
 @import "modules/utilities.js"
 @import "modules/rectangle.js"
@@ -9,15 +40,30 @@
 @import "modules/buttons.js"
 @import "modules/paint.js"
 
+let canvas = document.createElement('canvas'),
+	painter,
+	wgl;
 
 const fluidpaint = {
 	init() {
 		// fast references
 		this.content = window.find("content");
+		this.cvs = this.content.append(canvas);
+
+		this.dispatch({ type: "window.resize" });
+
+		wgl = WrappedGL.create(canvas);
+		painter = new Paint(canvas, wgl);
 	},
 	dispatch(event) {
+		let Self = fluidpaint,
+			el;
 		switch (event.type) {
 			case "window.open":
+				break;
+			case "window.resize":
+				// canvas
+				Self.cvs.prop({ width: window.innerWidth, height: window.innerHeight });
 				break;
 			case "open-help":
 				defiant.shell("fs -u '~/help/index.md'");
